d7c0185b7e79783d59ca3c39ce1a314e16d5f694
Ambre GONDRY <gondry.ambre@gmail.com>
2024-01-04 04:16:22 +0100

    quick fix html login form

--- src/views/LoginUI.js
+++ src/views/LoginUI.js
@@ -18,10 +18,10 @@ export default () => {
               <div class="card-body">
                 <form class="form-signin" data-testid="form-employee">
                   <h2 class="h3 mb-3 font-weight-normal">Employé</h1>
-                  <label for="inputEmail">Votre email</label>
-                  <input type="email"  autocomplete="current-email" data-testid="employee-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
-                  <label for="inputPassword">Mot de passe</label>
-                  <input type="password" autocomplete="current-password" data-testid="employee-password-input" class="form-control" placeholder="******" required>
+                  <label for="employeeInputEmail">Votre email</label>
+                  <input id="employeeInputEmail"type="email" autocomplete="current-email" data-testid="employee-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
+                  <label for="employeeInputPassword">Mot de passe</label>
+                  <input id="employeeInputPassword"  type="password" autocomplete="current-password" data-testid="employee-password-input" class="form-control" placeholder="******" required>
                   <button class="btn btn-lg btn-primary btn-block" data-testid="employee-login-button" style="background-color: #0E5AE5;" type="submit">Se connecter</button>
                 </form>
               </div>
@@ -32,10 +32,10 @@ export default () => {
               <div class="card-body">
                 <form class="form-signin" data-testid="form-admin">
                   <h2 class="h3 mb-3 font-weight-normal">Administration</h1>
-                  <label for="inputEmail">Votre email</label>
-                  <input type="email"  autocomplete="current-email" data-testid="admin-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
-                  <label for="inputPassword">Mot de passe</label>
-                  <input type="password" autocomplete="current-password" data-testid="admin-password-input" class="form-control" placeholder="******" required>
+                  <label for="adminInputEmail">Votre email</label>
+                  <input id="adminInputEmail" type="email" autocomplete="current-email" data-testid="admin-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
+                  <label for="adminInputPassword">Mot de passe</label>
+                  <input id="adminInputPassword" type="password" autocomplete="current-password" data-testid="admin-password-input" class="form-control" placeholder="******" required>
                   <button  type="submit" class="btn btn-lg btn-primary btn-block" data-testid="admin-login-button" style="background-color: #0E5AE5;">Se connecter</button>
                 </form>
               </div>
