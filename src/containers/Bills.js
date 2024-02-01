b2208c2ccd58514a63739fe37cad3cc0a4191d4e
Ambre GONDRY <gondry.ambre@gmail.com>
2024-02-01 13:08:23 +0100

    add rediraction on new bill onclick when click on iconmail

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -9,6 +9,8 @@ export default class {
     this.store = store
     const buttonNewBill = document.querySelector(`button[data-testid="btn-new-bill"]`)
     if (buttonNewBill) buttonNewBill.addEventListener('click', this.handleClickNewBill)
+    const iconNewBill = document.querySelector(`div[data-testid="icon-mail"]`)// test
+    if (iconNewBill) iconNewBill.addEventListener('click', this.handleClickNewBill)
     const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`)
     if (iconEye) iconEye.forEach(icon => {
       icon.addEventListener('click', () => this.handleClickIconEye(icon))
